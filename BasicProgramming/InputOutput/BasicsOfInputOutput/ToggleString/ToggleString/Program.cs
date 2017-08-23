27ee55ddc679cd6a1e60e200b678ba92098e9812
Klarizon Emar Motol <emurmotol@gmail.com>
2017-08-23 15:49:13 +0800

    up

---
+++ BasicProgramming/InputOutput/BasicsOfInputOutput/ToggleString/ToggleString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToggleString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] s = input.ToCharArray();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char letter = s[i];
+
+                if (char.IsUpper(letter))
+                {
+                    Console.Write(char.ToLower(letter));
+                    continue;
+                }
+                Console.Write(char.ToUpper(letter));
+            }
+            Console.ReadKey();
+        }
+    }
+}
